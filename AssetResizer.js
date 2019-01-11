2e4d9033917664b75d388d7da9fb259b2cfeea41
jcharbo <jcharbo@users.noreply.github.com>
2019-01-11 14:21:56 -0600

    Update AssetResizer.js

--- AssetResizer.js
+++ AssetResizer.js
@@ -1,7 +1,7 @@
 /* @flow */
 'use strict';
 
-import { requireNativeComponent, StyleSheet, Image } from 'react-native';
+import { NativeModules, StyleSheet, Image } from 'react-native';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
