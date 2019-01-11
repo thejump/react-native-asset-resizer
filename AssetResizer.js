15e50d5e0de4ec28dd31facb284aa9500e115f52
jcharbo <jcharbo@users.noreply.github.com>
2019-01-11 14:21:17 -0600

    Update AssetResizer.js

--- AssetResizer.js
+++ AssetResizer.js
@@ -5,6 +5,8 @@ import { requireNativeComponent, StyleSheet, Image } from 'react-native';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
-const RNAssetResizer = requireNativeComponent('RNAssetResizer', RNAssetResizer);
+//const RNAssetResizer = requireNativeComponent('RNAssetResizer', RNAssetResizer);
+
+const compressImg = NativeModules.RNAssetResizer;
 
 export default RNAssetResizer
